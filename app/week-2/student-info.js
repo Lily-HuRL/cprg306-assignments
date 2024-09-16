ae7cd625897c664cd2cfd81baac3865a054bb2cc
Ruili Hu <ruili.hu@edu.sait.ca>
2024-09-15 21:39:22 -0600

    Add Week 2 pages

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Ruili Hu</h1>
+      <p>
+        <Link href="https://github.com/Lily-HuRL/cprg306-assignments.git">Click to view on GitHub</Link>
+      </p>
+    </main>
+  );
+}
